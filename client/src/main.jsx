3efc93c893a090da51a9078b9789b44f639a13ba
Malaya Tiwari <sidzilegend@gmail.com>
2024-12-09 15:11:59 +0530

    update: setup routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,22 @@ import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
